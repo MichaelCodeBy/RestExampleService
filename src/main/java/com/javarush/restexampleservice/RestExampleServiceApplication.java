a43da827982dc998e46d576c84a2073d63163dbf
Michael <gomellc@gmail.com>
2023-04-18 09:30:40 +0300

    Starting write REST service

---
+++ src/main/java/com/javarush/restexampleservice/RestExampleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javarush.restexampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExampleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExampleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javarush/restexampleservice/RestExampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarush.restexampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
